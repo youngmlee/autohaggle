78751f0ecfc5c0e01db6792fe8cf3ece8c4ad6a8
Young Lee <ylee8575@gmail.com>
2017-11-08 13:03:46 -0800

    Removed .env file and used env mongodb_uri in create-app. #1

--- create-app.js
+++ create-app.js
@@ -11,7 +11,7 @@ module.exports = function createApp() {
   app.use(express.static(path.join(__dirname, 'public')))
 
   app.get('/autohaggle', async (req, res) => {
-    MongoClient.connect('mongodb://localhost/autohaggle', async (err, db) => {
+    MongoClient.connect(process.env.MONGODB_URI, async (err, db) => {
       const haggles = ahGateway(db.collection('haggles'))
       const displayed = await haggles.display()
       res.json(displayed)
@@ -23,7 +23,7 @@ module.exports = function createApp() {
   app.use(bodyParser.json())
 
   app.post('/autohaggle', async (req, res) => {
-    MongoClient.connect('mongodb://localhost/autohaggle', async (err, db) => {
+    MongoClient.connect(process.env.MONGODB_URI, async (err, db) => {
       const haggles = ahGateway(db.collection('haggles'))
       await haggles
         .createHaggle(req.body)
@@ -37,10 +37,10 @@ module.exports = function createApp() {
       from: 'autohagglebot@gmail.com',
       subject: 'AutoHaggle Price Inquiry',
       text: 'AutoHaggle inquiry',
-      html: 'Hello there! I have a potential customer for you interested in a ' + req.body.year + ' ' + req.body.make + ' ' + req.body.model + '. Additional details: ' + req.body.financing + req.body.credit + req.body.city + req.body.details + '. Could you give a price quote for such a car?',
+      html: 'Hello! A user has MANUALLY input your e-mail address to request a price quote through AutoHaggle.<br/>With that in mind, we hope you can participate in the AutoHaggle bidding process.<br/><br/>I have a potential customer for you interested in a ' + req.body.year + ' ' + req.body.make + ' ' + req.body.model + '.<br/><br/>Additional details:<br/>Financing: ' + req.body.financing + ' Credit estimation: ' + req.body.credit + ' City of residence: ' + req.body.city + '<br/><br/>Additional details: ' + req.body.details + '<br/><br/>Could you give me your lowest OTD price quote for such a car? Thank you for your time.',
     };
     sgMail.send(msg);
-    console.log('E-mail has been sent! ' + msg)
+    console.log('E-mail has been sent!')
   })
 
   return app
