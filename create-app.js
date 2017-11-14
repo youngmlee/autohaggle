cd5d1d3c9a40d9e3066ca6cd112665d884b96e71
Young Lee <ylee8575@gmail.com>
2017-11-14 13:01:03 -0800

    Changed the sender of original price inquiry e-mail.

--- create-app.js
+++ create-app.js
@@ -24,12 +24,12 @@ module.exports = function createApp() {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: req.body.demail,
-      from: 'auto-haggle.email',
+      from: 'Price-Inquiry@auto-haggle.email',
       subject: 'AutoHaggle Price Inquiry',
       text: 'AutoHaggle inquiry',
       html: 'Hello, this is an automated e-mail sent by AutoHaggle! A user on our site has MANUALLY input your e-mail address to request a price quote through AutoHaggle.<br/>With that in mind, we hope you can participate in the AutoHaggle bidding process by giving your best, honest price quote and honoring it when the time comes.<br/><br/>CUSTOMER REQUEST:<br/><br/>I have a potential customer for you interested in a ' + req.body.year + ' ' + req.body.color + ' ' + req.body.make + ' ' + req.body.model + ' ' + req.body.trim + '.<br/><br/>Additional details:<br/>Financing: ' + req.body.financing + '<br/>Credit estimation: ' + req.body.credit + '<br/>City of residence: ' + req.body.city + '<br/><br/>Additional details: ' + req.body.details + '<br/><br/>If you have this car, could you reply with your lowest "Out-The-Door" price quote for such a car? Thank you for your time.',
     };
-    sgMail.send(msg);
+    sgMail.sendMultiple(msg);
     console.log('E-mail has been sent!')
   })
 
